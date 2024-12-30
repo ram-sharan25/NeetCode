415604e4611f0190dd83aa56fccc297a774eb34b
Ram Sharan Rimal <rrimal@rimal24-mbp.local>
2024-12-29 22:24:10 -0600

    Day2of30:TopKfrequent using bucket sort

--- TopKFrequent.py
+++ TopKFrequent.py
@@ -4,7 +4,7 @@ import operator
 from heapq import nlargest
 
 
-nums = [7,7,7,2,3,1,1,9,9,8,8,8,8]
+nums = [7,7,7,2,3,1,1,9,9,8,8,8,8,14,8,8,8,8,8,8,8,8,8]
 k = 2
 
 class Solution:
@@ -18,19 +18,33 @@ class Solution:
             max_val = max(collection.items(), key=operator.itemgetter(1))[0]
             del collection[max_val]
             final_values.append(max_val)
-        print(final_values)
         return final_values
 
     # Heap Solution
     def top_k_frequent_heap(self, nums: List[int], k: int) -> List[int]:
         collection = defaultdict(int)
-        final_values = []
         for val in nums:
             collection[val]+=1
-
-        val = nlargest(k, collection.keys(), key=collection.get)
-        print("val",val)
+        final_values = nlargest(k, collection.keys(), key=collection.get)
         return final_values
+
+    #Bucket Sort
+    def top_k_frequent_bucket(self, nums: List[int], k: int) -> List[int]:
+        collection = defaultdict(int)
+        freq = [[] for i in range(len(nums)-1) ]
+        for val in nums:
+            collection[val]+=1
+        for val,cnt in collection.items():
+            freq[cnt].append(val)
+        final_output = []
+        for i in range(len(freq)-1,0,-1):
+            for num in freq[i]:
+                final_output.append(num)
+                if len(final_output)==k:
+                    return final_output
+        return []
+
+
 
 
 
@@ -39,6 +53,10 @@ out = eval.topKFrequent(nums,2)
 print(out)
 
 
-eval = Solution()
-out = eval.top_k_frequent_heap(nums,2)
-print(out)
+
+out1 = eval.top_k_frequent_heap(nums,2)
+print(out1)
+
+
+out2 = eval.top_k_frequent_bucket(nums,2)
+print(out2)
