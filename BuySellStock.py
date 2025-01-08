c25eaec37ed185edc7a7aa8aa42c10d1db5fc291
Ram Sharan Rimal <rrimal@rimal24-mbp.local>
2025-01-07 22:50:31 -0600

    Day9of30: Max Profit using Dynamic Programming

--- BuySellStock.py
+++ BuySellStock.py
@@ -3,8 +3,8 @@ from typing import List
 
 #prices = [10,1,5,6,7,1]
 #prices = [10,8,7,5,2]
-prices=[5,1,5,6,7,1,10]
-#prices=[2,1,2,1,0,1,2]
+#prices=[5,1,5,6,7,1,10]
+prices=[2,1,2,1,0,1,2]
 
 class Solution:
     # brute force
@@ -24,16 +24,22 @@ class Solution:
         while (l<r and r<len(prices)):
             left = prices[l]
             right = prices[r]
-            print(left,right)
             diff = right-left
             if diff > res:
                 res = diff
-            if left<=right:
-                r+=1
-            else:
-                l= r
-                r+=1
+            if left>right:
+               l = r
+            r+=1
 
+        return res
+    # dynamic prograaming
+    def maxProfit_dynamic(self, prices:List[int])->int:
+        res = 0
+        minBuy = prices[0]
+
+        for sell in prices:
+            res = max(res,sell-minBuy)
+            minBuy = min(minBuy,sell)
         return res
 
 eval = Solution()
@@ -42,3 +48,6 @@ print(out1)
 
 out2 = eval.maxProfit_two_pointer(prices)
 print(out2)
+
+out3 = eval.maxProfit_dynamic(prices)
+print(out3)
