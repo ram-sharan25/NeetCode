8c58607ca0130c6796da44c5ad543f4bd6b07e33
Ram Sharan Rimal <rrimal@rimal24-mbp.local>
2025-05-06 21:52:10 -0500

    Day 12 of 30:Pow(x,n)

--- pow_x_n.py
+++ pow_x_n.py
@@ -1,4 +1,5 @@
 class Solution:
+    # brute force
     def myPow(self, x: float, n: int) -> float:
         res = 1
         for i in range(abs(n)):
@@ -7,3 +8,22 @@ class Solution:
             else :
                 res/=x
         return res
+    # binary exponentation
+    def myPow_exp(self,x:float,n:int) ->int:
+        def helper(x,n):
+            print(x,n)
+            if x==0:
+                return 0
+            if n==0:
+                return 1
+            res = helper(x*x,n//2)
+            print("finally evaluated",res,x,n)
+            return res*x if n%2 else res
+        res = helper(x,abs(n))
+        print("IN the final",res)
+        return res if (n>=0) else 1/res
+
+x= 3 ; n = 6;
+
+eval = Solution ()
+out1 = eval.myPow_exp(x,n)
